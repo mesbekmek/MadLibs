a0d398577c8e142a4fb5191c731de77b0ad4b011
Mesfin <mesbekmek@gmail.com>
2015-06-07 16:12:21 -0400

    commiting again

--- MadLibs/MadLibs/ViewController.m
+++ MadLibs/MadLibs/ViewController.m
@@ -6,14 +6,54 @@
 //  Copyright (c) 2015 Mesfin. All rights reserved.
 //
 
+/*
+ Be kind to your Man-footed Women
+ For a duck may be somebody`s Fun,
+ Be kind to your Women in Ethiopia
+ Where the weather is always Fast.
+ 
+ You may think that this is the Man,
+ Well it is.
+ 
+ */
+
+
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *noun1;
+@property (weak, nonatomic) IBOutlet UITextField *nounPlural;
+
+@property (weak, nonatomic) IBOutlet UITextField *noun2;
+@property (weak, nonatomic) IBOutlet UITextField *place;
+@property (weak, nonatomic) IBOutlet UITextField *adjective;
+
+@property (weak, nonatomic) IBOutlet UITextField *noun3;
+
+@property (weak, nonatomic) IBOutlet UITextView *textView;
 
 @end
 
 @implementation ViewController
 
+- (IBAction)buttonTapped:(id)sender {
+
+    NSString *noun1 = self.noun1.text;
+    NSString *nounPlural = self.nounPlural.text;
+    NSString *noun2 = self.noun2.text;
+    NSString *place = self.place.text;
+    NSString *adjective = self.adjective.text;
+    NSString *noun3 = self.noun3.text;
+    
+    
+//    textView = @"Be kind to your @noun1-footed %@\nFor a duck may be somebody`s %@,\nBe kind to your %@ in Ethiopia.Where the weather is always %@.You may think that this is the %@, Well it is.";
+    
+    self.textView.text = [NSString stringWithFormat:@"Be kind to your %@-footed %@\n For a duck may be somebody`s %@,\n Be kind to your %@ in Ethiopia.\n Where the weather is always %@.\n You may think that this is the %@, Well it is.",noun1,nounPlural,noun2,place,adjective,noun3];
+    
+//    NSLog(@"Be kind to your %@-footed %@\nFor a duck may be somebody`s %@,\nBe kind to your %@ in Ethiopia.Where the weather is always %@.You may think that this is the %@, Well it is.",noun1,nounPlural,noun2,place,adjective,noun3);
+    
+
+}
 
 
 @end
